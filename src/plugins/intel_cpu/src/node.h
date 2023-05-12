c20329b7dfeca3c2fd9edd90996af4ec0da8d228
Maksim Kutakov <maksim.kutakov@intel.com>
2023-05-12 16:57:11 +0200

    Partitioned mem mngr

--- src/plugins/intel_cpu/src/node.h
+++ src/plugins/intel_cpu/src/node.h
@@ -192,6 +192,9 @@ public:
     const std::vector<EdgePtr> getParentEdgesAtPort(size_t idx) const;
     const std::vector<EdgePtr> getChildEdgesAtPort(size_t idx) const;
 
+    int inPlaceInputPort(int portIdx) const;
+    int inPlaceOutPort(int portIdx) const;
+
     bool isDropped() {
         return (isEdgesEmpty(childEdges) && isEdgesEmpty(parentEdges));
     }
@@ -358,7 +361,7 @@ public:
 
     PerfCount &PerfCounter() { return perfCounter; }
 
-    void resolveInPlaceEdges();
+    virtual void resolveInPlaceEdges();
 
     virtual void execute(dnnl::stream strm) = 0;
     void updateShapes();
